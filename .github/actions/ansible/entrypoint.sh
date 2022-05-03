492197a0feca7c75378858c0a161b63b76db6f1f
pvg1337 <pvg@i3d.net>
2022-05-03 22:21:08 +0200

    Added failure exit code to entrypoint.sh

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-ansible-galaxy install -r roles/requirements.yml
+#ansible-galaxy install -r roles/requirements.yml
+#echo $ANSIBLE_VAULT_PASSWORD >> .vault
+#ansible-playbook -i hosts_aws_ec2.yml site.yml --vault-password-file .vault
+#avoids locally storing on a mounted volume 
+#rm .vault
+
+#!/bin/bash
 
 echo $ANSIBLE_VAULT_PASSWORD >> .vault
 
-ansible-playbook -i hosts_aws_ec2.yml site.yml --vault-password-file .vault
+ansible-galaxy install -r roles/requirements.yml
 
-#avoids locally storing on a mounted volume 
-rm .vault
+if ! ansible-playbook -i <dynamic-inventory>.yml site.yml --vault-password-file .vault; then
+  echo "Ansible failed!"
+  rm .vault
+  exit 1
+else
+  rm .vault
+fi
